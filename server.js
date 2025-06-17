4192539bc16450bbb8308ca614965e78b2b0abfc
hamumeron <nekosuke.1012@outlook.jp>
2025-06-18 07:38:38 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3');
 const { nanoid } = require('nanoid');
-const path = require('path');
-
 const app = express();
 const db = new sqlite3.Database('./database.db');
 
@@ -13,9 +11,8 @@ app.use(express.static('public'));
 
 db.run(`CREATE TABLE IF NOT EXISTS codes (
   id TEXT PRIMARY KEY,
-  html TEXT,
-  css TEXT,
-  js TEXT
+  lang TEXT,
+  code TEXT
 )`);
 
 app.get('/', (req, res) => {
@@ -24,21 +21,18 @@ app.get('/', (req, res) => {
 
 app.post('/save', (req, res) => {
   const id = nanoid(8);
-  const { html, css, js } = req.body;
-  db.run(`INSERT INTO codes (id, html, css, js) VALUES (?, ?, ?, ?)`,
-    [id, html, css, js], (err) => {
-      if (err) return res.send('Error saving code');
-      res.redirect(`/view/${id}`);
-    });
-});
-
-app.get('/view/:id', (req, res) => {
-  const id = req.params.id;
-  db.get(`SELECT * FROM codes WHERE id = ?`, [id], (err, row) => {
-    if (!row) return res.send('Code not found');
-    res.render('code', { html: row.html, css: row.css, js: row.js });
+  const { lang, code } = req.body;
+  db.run(`INSERT INTO codes (id, lang, code) VALUES (?, ?, ?)`, [id, lang, code], (err) => {
+    if (err) return res.send('Error saving');
+    res.redirect(`/view/${id}`);
   });
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Running on port ${port}`));
+app.get('/view/:id', (req, res) => {
+  db.get(`SELECT * FROM codes WHERE id = ?`, [req.params.id], (err, row) => {
+    if (!row) return res.send('Not found');
+    res.render('view', { lang: row.lang, code: row.code });
+  });
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('Running'));
